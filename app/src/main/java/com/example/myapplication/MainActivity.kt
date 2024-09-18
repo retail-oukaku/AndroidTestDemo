3a536c7bde69b5280ba1536c909a9c96a61a29a5
retail-oukaku <ou_kaku@retail-ai.jp>
2024-09-18 15:16:57 +0900

    XMLファイルを追加 次画面に遷移機能を追加

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,32 +1,32 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        // 这里将 activity_main.xml 与 MainActivity 绑定
+        setContentView(R.layout.activity_main)
+    }
+
+    // 按钮点击事件触发跳转
+    fun openSecondActivity(view: View?) {
+        val intent = Intent(
+            this@MainActivity,
+            SecondActivity::class.java
+        )
+        startActivity(intent)
     }
 }
 
